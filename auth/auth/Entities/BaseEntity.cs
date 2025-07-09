fd7ef84a69f504d5340a73ee463254a8c462a833
William Ramirez <william.ramirez.paucar@udla.edu.ec>
2025-07-08 23:29:39 -0500

    Create BaseClass  to handle cleanly some basic attributes

--- auth/auth/Entities/BaseEntity.cs
+++ auth/auth/Entities/BaseEntity.cs
@@ -2,7 +2,7 @@
 {
     public abstract class BaseEntity
     {
-        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
         public string? CreatedBy { get; set; }
